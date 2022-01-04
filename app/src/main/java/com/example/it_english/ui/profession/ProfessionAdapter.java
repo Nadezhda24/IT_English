5327e7016a1f659037e7afc100a004d45e7fedad
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2022-01-04 21:11:52 +0300

    Создан шаблон фрагмента Profession

---
+++ app/src/main/java/com/example/it_english/ui/profession/ProfessionAdapter.java
@@ -0,0 +1,76 @@
+package com.example.it_english.ui.profession;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.it_english.R;
+import com.example.it_english.ui.terms.Term;
+import com.example.it_english.ui.terms.TermAdapter;
+
+import java.util.List;
+
+public class ProfessionAdapter extends RecyclerView.Adapter<ProfessionAdapter.ViewHolder>{
+
+    public interface OnProfessionClickListener{
+
+        void onProfessionClick(Profession profession, int position);
+    }
+
+    private final ProfessionAdapter.OnProfessionClickListener onClickListener;
+
+
+    private final LayoutInflater inflater;
+    private final List<Profession> professions;
+
+    ProfessionAdapter(Context context, List<Profession> professions, ProfessionAdapter.OnProfessionClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+        this.professions = professions;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ProfessionAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.template, parent, false);
+        return new ProfessionAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProfessionAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        Profession profession = professions.get(position);
+
+        holder.Name.setText(profession.getName());
+        holder.Icon.setImageResource(profession.getIcon());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickListener.onProfessionClick(profession, position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return professions.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        final TextView Name;
+        final ImageView Icon;
+
+        ViewHolder(View view){
+            super(view);
+            Name = view.findViewById(R.id.Name);
+            Icon = view.findViewById(R.id.Icon);
+        }
+    }
+
+}
--- app/src/main/java/com/example/it_english/ui/profession/ProfessionFragment.java
+++ app/src/main/java/com/example/it_english/ui/profession/ProfessionFragment.java
@@ -6,40 +6,61 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.it_english.R;
 import com.example.it_english.databinding.FragmentLanguagesBinding;
 import com.example.it_english.databinding.FragmentProfessionBinding;
+import com.example.it_english.databinding.FragmentTermsBinding;
 import com.example.it_english.ui.languages.LanguagesViewModel;
+import com.example.it_english.ui.terms.Term;
+import com.example.it_english.ui.terms.TermAdapter;
+
+import java.util.ArrayList;
 
 public class ProfessionFragment extends Fragment {
-    private ProfessionViewModel professionViewModel;
-    private FragmentProfessionBinding binding;
+    private FragmentTermsBinding binding;
+    ArrayList<Profession> Professions = new ArrayList<Profession>();
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        professionViewModel =
-                new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(ProfessionViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_profession, container, false);
 
-        binding = FragmentProfessionBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        setInitialData();
 
-        final TextView textView = binding.textProfession;
-        professionViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.List);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        ProfessionAdapter.OnProfessionClickListener professionClickListener = new ProfessionAdapter.OnProfessionClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onProfessionClick(Profession profession, int position) {
+
+                Toast.makeText(getActivity(), "Был выбран пункт " + profession.getName(),
+                        Toast.LENGTH_SHORT).show();
             }
-        });
-        return root;
+
+        };
+
+        ProfessionAdapter ProfessionAdapter = new ProfessionAdapter(getActivity(), Professions, professionClickListener);
+
+        recyclerView.setAdapter(ProfessionAdapter);
+
+        return view;
+
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void setInitialData(){
+        for (int i=0; i < 20; i++)
+            Professions.add(new Profession(1, "Профессия", "Профессия", R.drawable.ic_dashboard_black_24dp));
     }
+
+
 }
