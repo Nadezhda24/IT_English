553b998ff22943120d298a59ca3ee08288ea3057
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2022-01-04 20:25:49 +0300

    Создан шаблон фрагмента Term

---
+++ app/src/main/java/com/example/it_english/ui/terms/TermAdapter.java
@@ -0,0 +1,76 @@
+package com.example.it_english.ui.terms;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.it_english.R;
+
+import java.util.List;
+
+public class TermAdapter  extends RecyclerView.Adapter<TermAdapter.ViewHolder>{
+
+
+
+    public interface OnTermClickListener{
+        void onTaskClick(Term term, int position);
+    }
+
+    private final OnTermClickListener onClickListener;
+
+
+    private final LayoutInflater inflater;
+    private final List<Term> terms;
+
+    TermAdapter(Context context, List<Term> terms, OnTermClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+        this.terms = terms;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public TermAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.template, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TermAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        Term term = terms.get(position);
+
+        holder.Name.setText(term.getName());
+        holder.Icon.setImageResource(term.getIcon());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickListener.onTaskClick(term, position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return terms.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        final TextView Name;
+        final ImageView Icon;
+
+        ViewHolder(View view){
+            super(view);
+            Name = view.findViewById(R.id.Name);
+            Icon = view.findViewById(R.id.Icon);
+        }
+    }
+
+}
--- app/src/main/java/com/example/it_english/ui/terms/TermsFragment.java
+++ app/src/main/java/com/example/it_english/ui/terms/TermsFragment.java
@@ -2,24 +2,56 @@ package com.example.it_english.ui.terms;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.it_english.R;
 import com.example.it_english.databinding.FragmentTermsBinding;
 
+import java.util.ArrayList;
+
 public class TermsFragment extends Fragment {
-    private TermsViewModel termsViewModel;
+
     private FragmentTermsBinding binding;
+    ArrayList<Term> Terms = new ArrayList<Term>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        termsViewModel =
+        View view = inflater.inflate(R.layout.fragment_terms, container, false);
+
+        setInitialData();
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.List);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        TermAdapter.OnTermClickListener taskClickListener = new TermAdapter.OnTermClickListener() {
+            @Override
+            public void onTaskClick(Term term, int position) {
+
+                Toast.makeText(getActivity(), "Был выбран пункт " + term.getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        TermAdapter TermAdapter = new TermAdapter(getActivity(), Terms, taskClickListener);
+
+        recyclerView.setAdapter(TermAdapter);
+
+        return view;
+
+
+        /*termsViewModel =
                 new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(TermsViewModel.class);
 
         binding = FragmentTermsBinding.inflate(inflater, container, false);
@@ -32,12 +64,16 @@ public class TermsFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        return root;
+        return root;*/
+
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void setInitialData(){
+        for (int i=0; i < 20; i++)
+        Terms.add(new Term(1, "Программист", "Программист", R.drawable.ic_dashboard_black_24dp));
+
+
     }
+
+
 }
