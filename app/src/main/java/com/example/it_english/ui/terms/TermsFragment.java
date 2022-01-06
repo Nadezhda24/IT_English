c232be9724318cf33547faf59549af3867aa7521
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2022-01-06 11:52:21 +0300

    Парсинг json

--- app/src/main/java/com/example/it_english/ui/terms/TermsFragment.java
+++ app/src/main/java/com/example/it_english/ui/terms/TermsFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,10 @@ import android.widget.Toast;
 import com.example.it_english.HttpHandler;
 import com.example.it_english.R;
 import com.example.it_english.databinding.FragmentTermsBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -33,6 +38,7 @@ public class TermsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_terms, container, false);
 
         setInitialData();
+
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.List);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -60,8 +66,9 @@ public class TermsFragment extends Fragment {
         try {
             new GetData().execute().get();
         } catch (Exception e) { //TODO: сделать нормальное решение для catch
-            Terms.add(new Term(1, "Проверьте интернет и еще раз зайдите в раздел", "", R.drawable.ic_dashboard_black_24dp));
+            Terms.add(new Term(1, "Проверьте интернет и еще раз зайдите в раздел", "", R.drawable.warning));
         }
+
     }
 
     private class GetData extends AsyncTask<Void, Void, Void> {
@@ -75,7 +82,25 @@ public class TermsFragment extends Fragment {
         //выполняется после doInBackground
         @Override
         protected void onPostExecute(Void v) {
-            Terms.add(new Term(1, jsonRes, "Программист", R.drawable.ic_dashboard_black_24dp));
+            try {
+                JSONObject json = new JSONObject("{\"terms\": [{\"id\":\"2\",\"title\":\"term 2\",\"description\":\"Some description 2\",\"img\":null},{\"id\":\"3\",\"title\":\"term 3\",\"description\":\"Some description 3\",\"img\":null},{\"id\":\"4\",\"title\":\"term 5\",\"description\":\"desc 5\",\"img\":null},{\"id\":\"5\",\"title\":\"term 6\",\"description\":\"desc 6\",\"img\":\"terms_img_term_6.gif\"},{\"id\":\"6\",\"title\":\"term 7\",\"description\":\"desc 7\",\"img\":\"terms_img_term_7.gif\"}]}");
+                JSONArray arr = json.getJSONArray("terms");
+                for (int i=0; i < arr.length(); i++ ){
+                    JSONObject obj = arr.getJSONObject(i);
+                    int id = obj.getInt("id");
+                    String name = obj.getString("title");
+                    String description = obj.getString("description");
+                    String img = obj.getString("img");
+
+                    Terms.add(new Term(id, name, description, R.drawable.ic_dashboard_black_24dp));
+
+                }
+            } catch (JSONException e) {
+                Terms.add(new Term(1, "1", "2", R.drawable.ic_dashboard_black_24dp));
+                 e.printStackTrace();
+            }
+
+
             getActivity().runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
