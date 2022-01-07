77e1c1e2796be9e682af5ac82dce40fcc709b977
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2022-01-07 12:15:42 +0300

    Создание страниц для описания языков программирования

--- app/src/main/java/com/example/it_english/ui/languages/LanguagesFragment.java
+++ app/src/main/java/com/example/it_english/ui/languages/LanguagesFragment.java
@@ -1,5 +1,9 @@
 package com.example.it_english.ui.languages;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -10,14 +14,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.it_english.HttpHandler;
 import com.example.it_english.R;
+import com.example.it_english.databinding.FragmentLanguagesBinding;
 import com.example.it_english.databinding.FragmentTermsBinding;
+import com.example.it_english.ui.terms.Term;
+import com.example.it_english.ui.terms.TermActivity;
+import com.example.it_english.ui.terms.TermAdapter;
+import com.example.it_english.ui.terms.TermsFragment;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 
 public class LanguagesFragment  extends Fragment {
-    private FragmentTermsBinding binding;
+
+    private FragmentLanguagesBinding binding;
     ArrayList<Language> Languages = new ArrayList<Language>();
+    String jsonRes = null;
+    private static String  url =  "http://q90932z7.beget.tech/server.php?action=select_languages";
+    RecyclerView.Adapter LanguageAdapter;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -26,6 +47,7 @@ public class LanguagesFragment  extends Fragment {
 
         setInitialData();
 
+
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.List);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
@@ -33,13 +55,19 @@ public class LanguagesFragment  extends Fragment {
         LanguageAdapter.OnLanguageClickListener languageClickListener = new LanguageAdapter.OnLanguageClickListener() {
             @Override
             public void onLanguageClick(Language language, int position) {
-                Toast.makeText(getActivity(), "Был выбран пункт " + language.getName(),
-                        Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(getContext(), TermActivity.class);
+
+                intent.putExtra("id", language.getId());
+                intent.putExtra("name", language.getName());
+                intent.putExtra("description", language.getDescription());
+                intent.putExtra("icon", language.getIcon());
+
+                startActivity(intent);
             }
 
         };
 
-        LanguageAdapter LanguageAdapter = new LanguageAdapter(getActivity(), Languages, languageClickListener);
+        LanguageAdapter = new LanguageAdapter(getActivity(), Languages, languageClickListener);
 
         recyclerView.setAdapter(LanguageAdapter);
 
@@ -47,9 +75,68 @@ public class LanguagesFragment  extends Fragment {
 
     }
 
+
     private void setInitialData(){
-        for (int i=0; i < 20; i++)
-            Languages.add(new Language(1, "Язык программирования", "Язык программирования", R.drawable.ic_home_black_24dp));
+        try {
+            new LanguagesFragment.GetData().execute().get();
+        } catch (Exception e) { //TODO: сделать нормальное решение для catch
+            Languages.add(new Language(1, "Проверьте интернет и еще раз зайдите в раздел", "", R.drawable.warning));
+        }
+
+    }
+
+    private class GetData extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+            String jsonStr = sh.makeServiceCall(url);
+            jsonRes = jsonStr;
+            return null;
+        }
+        //выполняется после doInBackground
+        @Override
+        protected void onPostExecute(Void v) {
+            try {
+                JSONObject json = new JSONObject("{\"terms\": " + jsonRes + " }");
+                JSONArray arr = json.getJSONArray("terms");
+                for (int i=0; i < arr.length(); i++ ){
+                    JSONObject obj = arr.getJSONObject(i);
+                    int id = obj.getInt("id");
+                    String name = obj.getString("title");
+                    String description = obj.getString("description");
+                    String img = obj.getString("img");
+                    //Drawable drawable = new BitmapDrawable(getResources(), getBitmapFromURL("http://q90932z7.beget.tech/img/terms_img_term_6.jpg"));
+                    Languages.add(new Language(id, name, description, R.drawable.warning));
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    LanguageAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+    }
+
+    public Bitmap getBitmapFromURL(String src) {
+        try {
+            java.net.URL url = new java.net.URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
